424e9ed2e868475d481a08ba7542ac253514bd5d
Seyitan Oluwaseitan <syinka44@gmail.com>
2025-12-08 19:36:20 +0100

    updated files

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -25,7 +25,7 @@ provider "aws" {
 # SSH Key Pair
 resource "aws_key_pair" "ssh_key" {
   key_name   = var.key_name
- # public_key = var.ssh_public_key  
+  public_key = var.ssh_public_key  
 
   lifecycle {
     create_before_destroy = true
